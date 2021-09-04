732205d6c8b58b28f273ddd1cec42d4c02081119
Blitz.js CLI <noop@blitzjs.com>
2021-09-04 19:59:15 +0100

    migrate to new Blitz app

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,73 +1,40 @@
-import 'nprogress/nprogress.css';
-import 'utils/global.css';
-import NProgress from 'nprogress';
 import {
   AppProps,
+  ErrorBoundary,
   ErrorComponent,
-  useRouter,
   AuthenticationError,
   AuthorizationError,
-  Router,
-} from 'blitz';
-import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
-import { queryCache } from 'react-query';
-import { Box, ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
-import { theme } from 'theme';
-import { RecoilRoot } from 'recoil';
+  ErrorFallbackProps,
+  useQueryErrorResetBoundary,
+} from "blitz"
+import LoginForm from "app/auth/components/LoginForm"
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
-
-function App({ Component, pageProps }: AppProps) {
-  const getLayout = Component.getLayout || ((page) => page);
-  const router = useRouter();
+export default function App({ Component, pageProps }: AppProps) {
+  const getLayout = Component.getLayout || ((page) => page)
 
   return (
-    <RecoilRoot>
-      <ChakraProvider theme={theme} resetCSS>
-        <ColorModeProvider
-          value="dark"
-          options={{
-            initialColorMode: 'dark',
-            useSystemColorMode: false,
-          }}
-        >
-          <ErrorBoundary
-            FallbackComponent={RootErrorFallback}
-            resetKeys={[router.asPath]}
-            onReset={() => {
-              // This ensures the Blitz useQuery hooks will automatically refetch
-              // data any time you reset the error boundary
-              queryCache.resetErrorBoundaries();
-            }}
-          >
-            {getLayout(<Component {...pageProps} />)}
-          </ErrorBoundary>
-        </ColorModeProvider>
-      </ChakraProvider>
-    </RecoilRoot>
-  );
+    <ErrorBoundary
+      FallbackComponent={RootErrorFallback}
+      onReset={useQueryErrorResetBoundary().reset}
+    >
+      {getLayout(<Component {...pageProps} />)}
+    </ErrorBoundary>
+  )
 }
 
-function RootErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+function RootErrorFallback({ error, resetErrorBoundary }: ErrorFallbackProps) {
   if (error instanceof AuthenticationError) {
-    return <Box />;
+    return <LoginForm onSuccess={resetErrorBoundary} />
   } else if (error instanceof AuthorizationError) {
     return (
       <ErrorComponent
-        statusCode={(error as any).statusCode}
+        statusCode={error.statusCode}
         title="Sorry, you are not authorized to access this"
       />
-    );
+    )
   } else {
     return (
-      <ErrorComponent
-        statusCode={(error as any)?.statusCode || 400}
-        title={error?.message || error?.name}
-      />
-    );
+      <ErrorComponent statusCode={error.statusCode || 400} title={error.message || error.name} />
+    )
   }
 }
-
-export default App;
--- test/utils.tsx
+++ test/utils.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { RouterContext, BlitzRouter } from 'blitz';
-import { render as defaultRender } from '@testing-library/react';
-import { renderHook as defaultRenderHook } from '@testing-library/react-hooks';
+import { RouterContext, BlitzRouter, BlitzProvider } from "blitz"
+import { render as defaultRender } from "@testing-library/react"
+import { renderHook as defaultRenderHook } from "@testing-library/react-hooks"
 
-export * from '@testing-library/react';
+export * from "@testing-library/react"
 
 // --------------------------------------------------------------------------------
 // This file customizes the render() and renderHook() test functions provided
@@ -25,19 +24,18 @@ export * from '@testing-library/react';
 //   router: { pathname: '/my-custom-pathname' },
 // });
 // --------------------------------------------------
-export function render(
-  ui: RenderUI,
-  { wrapper, router, ...options }: RenderOptions = {},
-) {
+export function render(ui: RenderUI, { wrapper, router, dehydratedState, ...options }: RenderOptions = {}) {
   if (!wrapper) {
     // Add a default context wrapper if one isn't supplied from the test
     wrapper = ({ children }) => (
-      <RouterContext.Provider value={{ ...mockRouter, ...router }}>
-        {children}
-      </RouterContext.Provider>
-    );
+      <BlitzProvider dehydratedState={dehydratedState}>
+        <RouterContext.Provider value={{ ...mockRouter, ...router }}>
+          {children}
+        </RouterContext.Provider>
+      </BlitzProvider>
+    )
   }
-  return defaultRender(ui, { wrapper, ...options });
+  return defaultRender(ui, { wrapper, ...options })
 }
 
 // --------------------------------------------------
@@ -53,26 +51,31 @@ export function render(
 // --------------------------------------------------
 export function renderHook(
   hook: RenderHook,
-  { wrapper, router, ...options }: RenderHookOptions = {},
+  { wrapper, router, dehydratedState,...options }: RenderHookOptions = {}
 ) {
   if (!wrapper) {
     // Add a default context wrapper if one isn't supplied from the test
     wrapper = ({ children }) => (
-      <RouterContext.Provider value={{ ...mockRouter, ...router }}>
-        {children}
-      </RouterContext.Provider>
-    );
+      <BlitzProvider dehydratedState={dehydratedState}>
+        <RouterContext.Provider value={{ ...mockRouter, ...router }}>
+          {children}
+        </RouterContext.Provider>
+      </BlitzProvider>
+    )
   }
-  return defaultRenderHook(hook, { wrapper, ...options });
+  return defaultRenderHook(hook, { wrapper, ...options })
 }
 
-export const mockRouter: BlitzRouter = {
-  basePath: '',
-  pathname: '/',
-  route: '/',
-  asPath: '/',
+export const mockRouter: BlitzRouter =     {
+  basePath: "",
+  pathname: "/",
+  route: "/",
+  asPath: "/",
   params: {},
   query: {},
+  isReady: true,
+  isLocaleDomain: false,
+  isPreview: false,
   push: jest.fn(),
   replace: jest.fn(),
   reload: jest.fn(),
@@ -85,14 +88,12 @@ export const mockRouter: BlitzRouter = {
     emit: jest.fn(),
   },
   isFallback: false,
-};
+}
 
-type DefaultParams = Parameters<typeof defaultRender>;
-type RenderUI = DefaultParams[0];
-type RenderOptions = DefaultParams[1] & { router?: Partial<BlitzRouter> };
+type DefaultParams = Parameters<typeof defaultRender>
+type RenderUI = DefaultParams[0]
+type RenderOptions = DefaultParams[1] & { router?: Partial<BlitzRouter>, dehydratedState?: unknown }
 
-type DefaultHookParams = Parameters<typeof defaultRenderHook>;
-type RenderHook = DefaultHookParams[0];
-type RenderHookOptions = DefaultHookParams[1] & {
-  router?: Partial<BlitzRouter>;
-};
+type DefaultHookParams = Parameters<typeof defaultRenderHook>
+type RenderHook = DefaultHookParams[0]
+type RenderHookOptions = DefaultHookParams[1] & { router?: Partial<BlitzRouter>, dehydratedState?: unknown }
